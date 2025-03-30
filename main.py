14a2246d65edaa2d5a1677118b0398cdd6cf812f
hfan0216 <72716123+hfan0216@users.noreply.github.com>
2025-03-30 18:21:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,12 @@ def data_get(product_id):
 #data = data_get(product_id=1, url='http://域名+接口地址')
 
 response = requests.get('https://apim-northwindshoes123.azure-api.net/northwindshoes01/api/Products/1')
+print(response)
+# HTTP 响应状态码为 200 表示请求成功，服务器成功处理了请求
+if response.status_code == 200:
+    # 获取字典中的Data值
+    print("ok")
+else:
+    # HTTP 响应状态码不为 200 时，提示“URL未正常响应请求”
+    raise Exception('URL未正常响应请求')
+return value
